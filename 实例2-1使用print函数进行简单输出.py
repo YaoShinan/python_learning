12e5c29caf6f24f133aeffc6f9aeae1af4856805
Twinklin <163401323+YaoShinan@users.noreply.github.com>
2024-07-28 21:25:18 +0800

    Add files via upload

---
+++ 实例2-1使用print函数进行简单输出.py
@@ -0,0 +1,10 @@
+a=100 #变量a,值为100
+b=50 #变量b,值为50
+print(100)
+print(a) #输出变量a的值
+print(a*b) #输出a*b的运算结果
+
+print('欢迎来到python')
+print("欢迎来到python")
+print('''欢迎来到python''')
+print("""欢迎来到python""")
