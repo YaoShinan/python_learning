d218dee51af1d12b17e9e753ab1c7d146405030a
Flame <163401323+YaoShinan@users.noreply.github.com>
2024-10-31 12:00:30 +0800

    Add files via upload

---
+++ 示例5-22结构的模式匹配.py
@@ -0,0 +1,11 @@
+# data=eval(input('请输入要匹配的数据:'))
+# match data:
+#     case {'name':'lily','age':20}:
+#         print('字典')
+#     case [10,20,30]:
+#         print('列表')
+#     case (10,20,30):
+#         print('元组')
+#     case _:
+#         print('相当于多重if中的else')
+#only practical in python3.11
