e6809f88fbd3b534dd04c3dedcf5f31b5051db08
Flame <163401323+YaoShinan@users.noreply.github.com>
2024-11-02 10:11:46 +0800

    Add files via upload

---
+++ 示例5-24同步迭代.py
@@ -0,0 +1,13 @@
+# fruits={'apple','orange','pear','grape'}
+# counts=[10,3,4,5]
+# for f,c in zip(fruits,counts):
+#     match f,c:
+#         case 'apple',10:
+#             print('10个苹果')
+#         case 'orange',3:
+#             print('3个橙子')
+#         case 'pear',4:
+#             print('四个梨子')
+#         case 'grape',5:
+#             print('5串葡萄')
+#only pracitical in python3.11
